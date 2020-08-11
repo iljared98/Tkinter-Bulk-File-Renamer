da3b001a8d08a8ec7f954b4b47048d1298937cb7
iljared98 <52998588+iljared98@users.noreply.github.com>
2020-08-10 23:56:29 -0500

    Update main.py
    Refactoring

--- main.py
+++ main.py
@@ -1,129 +1,90 @@
-# Author      : Isaiah Jared
-# Description : Bulk renames all files with a certain
-#               extension via a set pattern. User will
-#               enter a base name string which will
-#               then be incremented (i.e. myimage1.png, myimage2.png)
-#               for each file of the same type in the directory.
-
 from tkinter import *
 from tkinter import filedialog, messagebox
 import os
 import sys
 import glob
 
-window = Tk()
-window.title("Bulk File Renamer")
-window.minsize(425,300)
+class Application:
+    def __init__(self, master):
+        self.master = master
+        master.title("Bulk File Renamer")
+        root.minsize(425, 300)
+
+        self.menu = Menu(root)
+        root.config(menu=self.menu)
+
+        self.fileMenu = Menu(self.menu, tearoff=0)
+        self.fileMenu.add_command(label="Exit", command=self.progExit)
+        self.menu.add_cascade(label="File", menu=self.fileMenu)
+
+        self.helpMenu = Menu(self.menu, tearoff=0)
+        self.helpMenu.add_command(label="About Me", command=self.aboutMe)
+        self.helpMenu.add_command(label="Instructions", command=self.progHelp)
+        self.menu.add_cascade(label="Help", menu=self.helpMenu)
+
+        self.label1 = Label(root, text="Desired Filename")
+        self.label1.grid(column=0, row=1, pady=(15, 0))
+
+        self.entry1 = Entry(root)
+        self.entry1.grid(column=0, row=2, padx=10)
+
+        self.label2 = Label(root, text="Old Extension")
+        self.label2.grid(column=1, row=1, pady=(15, 0))
+
+        self.entry2 = Entry(root)
+        self.entry2.grid(column=1, row=2, padx=10)
 
 
 
-# FILENAME
+        self.label3 = Label(root, text="New Extension")
+        self.label3.grid(column=2, row=1, pady=(15, 0))
 
-def storeVal1():
-  global val1
-  val1 = entry1.get()
+        self.entry3 = Entry(root)
+        self.entry3.grid(column=2, row=2, padx=10)
 
-# OLD EXTENSION
-def storeVal2():
-  global val2
-  val2 = entry2.get()
+        self.button4 = Button(root, text="Choose Directory", command=self.dirChoice)
+        self.button4.grid(column=0, row=5, pady=(140, 5))
 
-# NEW EXTENSION (OVERWRITES OLD!)
-def storeVal3():
-  global val3
-  val3 = entry3.get()
+        self.button5 = Button(root, text="Rename files", command=self.bulkRename)
+        self.button5.grid(column=2, row=5, pady=(140, 5))
 
-def dirChoice():
-  global path
-  path = filedialog.askdirectory() + '/'
+    def progExit(self):
+        sys.exit(0)
 
-def progExit():
-    sys.exit(0)
+    def aboutMe(self):
+        messagebox.showinfo('About Me', 'Author: Isaiah Jared \n\nhttps://github.com/iljared98/', )
 
-def aboutMe():
-    messagebox.showinfo('About Me', 'Author: Isaiah Jared \n\nhttps://github.com/iljared98/', )
+    def progHelp(self):
+        messagebox.showinfo('Help','Using the program:\n\n1. Enter your desired name template then click Confirm.\n2. Enter the file extension you want to rename. Click Confirm\n3. Enter a new extension if you want to replace the old, if not, simply re-enter the old extension. Click Confirm.\n4. Choose your directory with the files you want to rename.\n5. "Click Rename Files"')
 
-def progHelp():
-    messagebox.showinfo('Help','Using the program:\n\n1. Enter your desired name template then click Confirm.\n2. Enter the file extension (no .) you want to rename. Click Confirm\n3. Enter a new extension if you want to replace the old, if not, simply re-enter the old extension. Click Confirm.\n4. Choose your directory with the files you want to rename.\n5. "Click Rename Files"')
+    def dirChoice(self):
+        self.path = filedialog.askdirectory() + '/'
 
+    def bulkRename(self):
+        self.filename = self.entry1.get()
+        self.oldExt = self.entry2.get()
+        self.newExt = self.entry3.get()
 
-def bulkRename():
-  filelist = glob.glob(path + "*.{}".format(val2))
-  count = 0
-  for file in filelist:
-      filename = os.path.split(file)
-      os.path.splitext(path)[0]+'{}'.format(val2)
-      new_filename = path + val1 + str(count + 1) + ".{}".format(val3)
-      os.rename(path+filename[1], new_filename)
-      count = count + 1
+        if "." in self.oldExt or "." in self.newExt:
+            messagebox.showerror("Input Error",'Please only enter the extension name (ex. PNG).')
+        else:
+            if len(self.filename) == 0 or len(self.oldExt) == 0 or len(self.newExt) == 0:
+                messagebox.showerror("Input Error",'One of your input fields is empty.')
+            else:
+                try:
+                    filelist = glob.glob(self.path + "*.{}".format(self.oldExt))
+                    i = 0
+                    for file in filelist:
+                        old_filename = os.path.split(file)
+                        os.path.splitext(self.path)[0] + '{}'.format(self.oldExt)
+                        new_filename = self.path + self.filename + str(i + 1) + ".{}".format(self.newExt)
+                        os.rename(self.path + old_filename[1], new_filename)
+                        i = i + 1
 
-
-
-
-# Menu bar #
-
-menu = Menu(window)
-window.config(menu=menu)
-
-fileMenu = Menu(menu,tearoff=0)
-fileMenu.add_command(label="Exit",command=progExit)
-menu.add_cascade(label="File",menu=fileMenu)
-
-helpMenu = Menu(menu,tearoff=0)
-helpMenu.add_command(label="About Me",command=aboutMe)
-helpMenu.add_command(label="Instructions",command=progHelp)
-menu.add_cascade(label="Help",menu=helpMenu)
-
-##############################################################
-##############################################################
-
-label1 = Label(window, text="Desired Filename")
-label1.grid(column=0, row=1,pady=(15,0))
-#label1.pack()
-
-entry1 = Entry(window)
-entry1.grid(column=0, row=2,padx=10)
-#entry1.pack()
-
-button1 = Button(window, text="Confirm",command=storeVal1)
-button1.grid(column=0, row=3)
-#button1.pack()
-
-label2 = Label(window, text="Old Extension")
-label2.grid(column=1, row=1,pady=(15,0))
-#label2.pack()
+                except ValueError:
+                    messagebox.showerror('Error','Please double check your inputs and try again.')
+root = Tk()
+my_gui = Application(root)
+root.mainloop()
 
 
-entry2 = Entry(window)
-entry2.grid(column=1, row=2,padx=10)
-#entry2.pack()
-
-button2 = Button(window, text="Confirm",command=storeVal2)
-button2.grid(column=1, row=3)
-#button2.pack()
-
-label3 = Label(window, text="New Extension")
-label3.grid(column=2, row=1,pady=(15,0))
-#label3.pack()
-
-entry3 = Entry(window)
-entry3.grid(column=2, row=2,padx=10)
-#entry3.pack()
-
-button3 = Button(window, text="Confirm",command=storeVal3)
-button3.grid(column=2, row=3)
-#button3.pack()
-
-
-
-# Change this to a folder icon or something later on
-
-button4 = Button(window, text="Choose Directory", command=dirChoice)
-button4.grid(column=0, row=5,pady=(140,5))
-#button4.pack()
-
-button5 = Button(window, text="Rename files",command=bulkRename)
-button5.grid(column=2, row=5,pady=(140,5))
-#button5.pack()
-
-window.mainloop()
